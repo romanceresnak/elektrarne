b565ffa714b595f5c715ae09f1d54aa4ea953f3c
Roman Ceresnak <romanceresnak@windowslive.com>
2025-01-17 10:22:17 +0100

    Backend issue

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-bucket"
+    bucket = "my-terraform-state-bucket-rc-el"
     key    = "state/dev.tfstate"
     region = "eu-west-1"
   }
